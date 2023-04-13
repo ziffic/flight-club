7b34f13689e3d3da7e54db8cc3e4cb69aba718bf
David Yrigoyen <dave@dryweb.com>
2023-04-12 20:33:52 -0700

    lay over code added

--- flight_search.py
+++ flight_search.py
@@ -1,5 +1,6 @@
 import requests
 from flight_data import FlightData
+from pprint import pprint
 import connect
 
 TEQUILA_ENDPOINT = connect.KIWI_ENDPOINT
@@ -7,16 +8,24 @@ TEQUILA_API_KEY = connect.KIWI_API_KEY
 
 
 class FlightSearch:
-    def get_destination_code(self, city_name):
+    def __int__(self):
+        self.city_codes = []
+
+    def get_destination_codes(self, city_names):
+        print("get destination codes triggered")
         location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
         headers = {"apikey": TEQUILA_API_KEY}
-        query = {"term": city_name, "location_types": "city"}
-        response = requests.get(url=location_endpoint, headers=headers, params=query)
-        results = response.json()["locations"]
-        code = results[0]["code"]
-        return code
+        for city in city_names:
+            query = {"term": city, "location_types": "city"}
+            response = requests.get(url=location_endpoint, headers=headers, params=query)
+            results = response.json()["locations"]
+            code = results[0]["code"]
+            self.city_codes.append(code)
+
+        return self.city_codes
 
     def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        print(f"Check flights triggered for {destination_city_code}")
         headers = {"apikey": TEQUILA_API_KEY}
         query = {
             "fly_from": origin_city_code,
@@ -40,17 +49,40 @@ class FlightSearch:
         try:
             data = response.json()["data"][0]
         except IndexError:
-            print(f"No flights found for {destination_city_code}.")
-            return None
-
-        flight_data = FlightData(
-            price=data["price"],
-            origin_city=data["route"][0]["cityFrom"],
-            origin_airport=data["route"][0]["flyFrom"],
-            destination_city=data["route"][0]["cityTo"],
-            destination_airport=data["route"][0]["flyTo"],
-            out_date=data["route"][0]["local_departure"].split("T")[0],
-            return_date=data["route"][1]["local_departure"].split("T")[0]
-        )
-        print(f"{flight_data.destination_city}: £{flight_data.price}")
+            query["max_stopovers"] = 1
+            response = requests.get(
+                url=f"{TEQUILA_ENDPOINT}/v2/search",
+                headers=headers,
+                params=query,
+            )
+            try:
+                data = response.json()["data"][0]
+                pprint(data)
+            except IndexError:
+                return None
+            else:
+                flight_data = FlightData(
+                    price=data["price"],
+                    origin_city=data["route"][0]["cityFrom"],
+                    origin_airport=data["route"][0]["flyFrom"],
+                    destination_city=data["route"][1]["cityTo"],
+                    destination_airport=data["route"][1]["flyTo"],
+                    out_date=data["route"][0]["local_departure"].split("T")[0],
+                    return_date=data["route"][2]["local_departure"].split("T")[0],
+                    stop_overs=1,
+                    via_city=data["route"][0]["cityTo"]
+                )
+                print(f"{flight_data.destination_city}: {flight_data.price}")
+                return flight_data
+        else:
+            flight_data = FlightData(
+                price=data["price"],
+                origin_city=data["route"][0]["cityFrom"],
+                origin_airport=data["route"][0]["flyFrom"],
+                destination_city=data["route"][0]["cityTo"],
+                destination_airport=data["route"][0]["flyTo"],
+                out_date=data["route"][0]["local_departure"].split("T")[0],
+                return_date=data["route"][1]["local_departure"].split("T")[0]
+            )
+        print(f"{flight_data.destination_city}: {flight_data.price}")
         return flight_data
--- main.py
+++ main.py
@@ -4,36 +4,47 @@ from flight_search import FlightSearch
 from notification_manager import NotificationManager
 
 
+ORIGIN_CITY_IATA = "LON"
+
 data_manager = DataManager()
-sheet_data = data_manager.get_destination_data()
 flight_search = FlightSearch()
 notification_manager = NotificationManager()
 
-ORIGIN_CITY_IATA = "LON"
+sheet_data = data_manager.get_destination_data()
 
 if sheet_data[0]["iataCode"] == "":
-    for row in sheet_data:
-        row["iataCode"] = flight_search.get_destination_code(row["city"])
-    data_manager.destination_data = sheet_data
+    city_names = [row["city"] for row in sheet_data]
+    data_manager.city_codes = flight_search.get_destination_codes(city_names)
     data_manager.update_destination_codes()
+    sheet_data = data_manager.get_destination_data()
+
+destinations = {
+    data["iataCode"]: {
+        "id": data["id"],
+        "city": data["city"],
+        "price": data["lowestPrice"]
+    } for data in sheet_data}
 
 tomorrow = datetime.now() + timedelta(days=1)
-six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+six_month_from_today = datetime.now() + timedelta(days=6 * 30)
 
-for destination in sheet_data:
+for destination_code in destinations:
     flight = flight_search.check_flights(
         ORIGIN_CITY_IATA,
-        destination["iataCode"],
+        destination_code,
         from_time=tomorrow,
         to_time=six_month_from_today
     )
-
     if flight is None:
         continue
 
-    # if flight.price < destination["lowestPrice"]:
-    #     notification_manager.send_sms(
-    #         message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} "
-    #                 f"to {flight.destination_city}-{flight.destination_airport}, "
-    #                 f"from {flight.out_date} to {flight.return_date}."
-    #     )
+    if flight.price < destinations[destination_code]["price"]:
+        message = f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} " \
+                  f"to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} " \
+                  f"to {flight.return_date}."
+
+        if flight.stop_overs > 0:
+            message += f"\nFlight has {flight.stop_overs} stop over, via {flight.via_city}."
+            print(message)
+
+        notification_manager.send_sms(message)
